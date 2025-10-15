cefba01eb5f4aa40575574651969dfa126973d5c
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2025-10-15 17:54:05 +0200

    fix indentation and typos

--- predict/prediction.py
+++ predict/prediction.py
@@ -162,26 +162,26 @@ def predict_vis(args: argparse.Namespace, sky_model: WSCleanModel, backend: str)
         logging.info(f"Rank {ht_uvw.comm.rank}: UVW local shape: {ht_uvw.lshape}, global shape: {ht_uvw.gshape}")
 
         # In this particular example, we know we have a single FIELD and DATA_DESCRIPTION for all MAIN_* partitions.
-        field_ds = xds_from_storage_table(f"{args.store}::FIELD").compute()
-        phase_dir = field_ds[0].PHASE_DIR.values[0][0]
+        field_ds = xds_from_storage_table(f"{args.store}::FIELD")[0].compute()
+        phase_dir = field_ds.PHASE_DIR.values[0][0]
 
         # Create frequency array
-       nchan = args.dimensions["chan"]
-       ht_frequency = ht.linspace(0.856e9, 2 * 0.856e9, nchan, split=None)
+        nchan = args.dimensions["chan"]
+        ht_frequency = ht.linspace(0.856e9, 2 * 0.856e9, nchan, split=None)
 
-       # Convert radec to lm coordinates
-       ht_lm = heat_radec_to_lm(ht_radec, phase_dir)
+        # Convert radec to lm coordinates
+        ht_lm = heat_radec_to_lm(ht_radec, phase_dir)
 
-       # Call the Heat prediction kernel
-       logging.info("Starting Heat visibility prediction...")
-       ht_vis = heat_wsclean_predict(ht_uvw, ht_lm, ht_source_type, ht_flux, ht_spi, 
-                                     ht_log_poly, ht_ref_freq, ht_gauss_shape, ht_frequency)
+        # Call the Heat prediction kernel
+        logging.info("Starting Heat visibility prediction...")
+        ht_vis = heat_wsclean_predict(ht_uvw, ht_lm, ht_source_type, ht_flux, ht_spi, 
+                                        ht_log_poly, ht_ref_freq, ht_gauss_shape, ht_frequency)
 
-       # Save the resulting distributed tensor to a Zarr store
-       logging.info("Computation complete. Writing output to %s", args.output_store)
-       ht_vis.save(args.output_store, overwrite=True)
-       ht_vis.comm.Barrier()
-       logging.info("Output successfully written.")
+        # Save the resulting distributed tensor to a Zarr store
+        logging.info("Computation complete. Writing output to %s", args.output_store)
+        ht_vis.save(args.output_store, overwrite=True)
+        ht_vis.comm.Barrier()
+        logging.info("Output successfully written.")
 
     else:
         raise ValueError(f"Unknown backend: {backend}")
