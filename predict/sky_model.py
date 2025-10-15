d5b57358cd88d93e8701108b7e111097f4123488
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2025-10-15 05:06:43 +0200

    refactor: Decouple sky model generation from Dask, int codes for source type

--- predict/sky_model.py
+++ predict/sky_model.py
@@ -1,7 +1,9 @@
 from argparse import Namespace
 from collections import namedtuple
 
-import dask.array as da
+# import dask.array as da
+import numpy as np
+from numpy.random import default_rng
 
 WSCleanModel = namedtuple(
     "WSCleanModel",
@@ -11,19 +13,23 @@ WSCleanModel = namedtuple(
 
 def generate_sky_model(args: Namespace) -> WSCleanModel:
     sdims = args.dimensions["source"]
-    schunks = args.chunks["source"]
+    # chunks not used in this implementation
+    # Initialise a random number generator
+    rng = default_rng()
 
     # MeerKAT centre frequency
-    ref_freq = da.full(sdims, (0.856e9 + 2 * 0.856e9) / 2, chunks=schunks)
-    source_type = da.full(sdims, "POINT", chunks=schunks, dtype="<U5")
-    flux = da.random.random(sdims, chunks=schunks) * 1e-4
-    spi = da.random.random((sdims, 2), chunks=schunks) * 1e-3
+    ref_freq = np.full(sdims, (0.856e9 + 2 * 0.856e9) / 2)
+    # Use integer codes for masking: 0 for POINT, 1 for GAUSSIAN
+    source_type = np.zeros(sdims, dtype=np.int8)
+    flux = rng.random(sdims) * 1e-4
+    spi = rng.random((sdims, 2)) * 1e-3
+
 
     # six degrees around zero
-    radec = da.random.random((sdims, 2), chunks=(schunks, 2))
-    radec = da.deg2rad(6.0 * (radec - 0.5))
-    log_si = da.full(sdims, False, chunks=schunks)
-    gauss_shape = da.zeros((sdims, 3), chunks=schunks)
+    radec = rng.random((sdims, 2))
+    radec = np.deg2rad(6.0 * (radec - 0.5))
+    log_si = np.full(sdims, False)
+    gauss_shape = np.zeros((sdims, 3))
 
     return WSCleanModel(
         source_type,
