420feb66584a8b7129dd6667f55dfaeafb67281b
Claudia Comito <39374113+ClaudiaComito@users.noreply.github.com>
2025-10-16 17:55:57 +0200

    set dtypes and seed

--- predict/sky_model.py
+++ predict/sky_model.py
@@ -15,21 +15,22 @@ def generate_sky_model(args: Namespace) -> WSCleanModel:
     sdims = args.dimensions["source"]
     # chunks not used in this implementation
     # Initialise a random number generator
-    rng = default_rng()
+    seed = 42
+    rng = default_rng(seed)
 
     # MeerKAT centre frequency
-    ref_freq = np.full(sdims, (0.856e9 + 2 * 0.856e9) / 2)
+    ref_freq = np.full(sdims, (0.856e9 + 2 * 0.856e9) / 2, dtype=np.float32)
     # Use integer codes for masking: 0 for POINT, 1 for GAUSSIAN
-    source_type = np.zeros(sdims, dtype=np.int8)
-    flux = rng.random(sdims) * 1e-4
-    spi = rng.random((sdims, 2)) * 1e-3
+    source_type = np.zeros(sdims, dtype=np.int32)
+    flux = rng.random(sdims, dtype=np.float32) * 1e-4
+    spi = rng.random((sdims, 2), dtype=np.float32) * 1e-3
 
 
     # six degrees around zero
-    radec = rng.random((sdims, 2))
+    radec = rng.random((sdims, 2), dtype=np.float32)
     radec = np.deg2rad(6.0 * (radec - 0.5))
     log_si = np.full(sdims, False)
-    gauss_shape = np.zeros((sdims, 3))
+    gauss_shape = np.zeros((sdims, 3), dtype=np.float32)
 
     return WSCleanModel(
         source_type,
